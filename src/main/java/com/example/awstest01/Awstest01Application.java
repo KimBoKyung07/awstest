a57d9210eb7e82208f3ca810cc8a282a122e352c
BoKyungKim <qhzud1207@gmail.com>
2026-03-26 19:34:06 +0900

    awstest01

---
+++ src/main/java/com/example/awstest01/Awstest01Application.java
@@ -0,0 +1,13 @@
+package com.example.awstest01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Awstest01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Awstest01Application.class, args);
+    }
+
+}
